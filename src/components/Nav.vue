39f07b5d2feb7819921eed0905884a8bf78df18e
menghuiguli <fujia0@qq.com>
2021-12-03 11:39:35 +0800

    init

---
+++ src/components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul class="nav">
+            <li><a href="">首页</a></li>
+            <li><a href="">热映</a></li>
+            <li><a href="">分类</a></li>
+            <li><a href="">我的</a></li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const activeIndex = ref('1')
+    const handleSelect = (key, keyPath) => {
+      console.log(key, keyPath)
+    }
+    return {
+      activeIndex,
+      handleSelect,
+    }
+  },
+})
+</script>
+
+<style scoped>
+    .nav{
+        list-style: none;
+    }
+    .nav li{
+        display: flex;
+    }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import {createRouter,createWebHistory} from 'vue-router'
+
+const routes = [
+    {
+        path:"/",
+        redirect:'/home'
+    },
+    {
+        path:'/home',
+        name:'Home',
+        component: ()=> import("../components/Home.vue")
+    },
+    {
+        path:'/cateogry',
+        name:'Category',
+        component: ()=> import("../components/Home.vue")
+    }
+]
+
+const router = createRouter({
+    history:createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
